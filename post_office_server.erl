f31d0350cc5f79b415772d983e7590370e636c67
sowakarol <karosowa@student.agh.edu.pl>
2018-01-09 23:14:56 +0100

    Added empty files representing server and random_data generator

---
+++ post_office_server.erl
@@ -0,0 +1,5 @@
+-module(post_office_server).
+-compile([export_all]).
+
+
+
---
+++ random_data.erl
@@ -0,0 +1,10 @@
+-module(random_data).
+-compile(export_all).
+
+
+% to add to average data some possible delays
+
+
+%generate_consumer_per_day() ->
+
+
